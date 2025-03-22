d66084e519855c79a6bfa60db04b69123234955e
Jan Kazubski <jkazubski@icloud.com>
2025-03-22 16:28:20 +0100

    created Fork struct with methods to acquire and release it

--- main.cpp
+++ main.cpp
@@ -1,22 +1,48 @@
 #include <iostream>
+#include <vector>
+#include <atomic>
 
 using namespace std;
+
+struct Fork {
+private:
+    std::atomic<bool> available;
+
+public:
+    Fork() : available(true) { }
+
+    bool acquire() {
+        bool expected = true;
+        return available.compare_exchange_strong(expected, false);
+    }
+
+    void release() {
+        available.store(true);
+    }
+};
 
 struct Philosopher {
     static int next_id;
     int id;
-    bool isEating;
-    
-    Philosopher() : id(next_id++), isEating(false) { }
+    Fork* leftFork;
+    Fork* rightFork;
+
+    Philosopher(Fork* left, Fork* right)
+        : id(next_id++), leftFork(left), rightFork(right) { }
 
     void eat() {
-        isEating = true;
-        cout << "Philosopher " << id << " is eating." << endl;
+        cout << "Philosopher " << id << " starts eating." << endl;
+        sleep();
+        cout << "Philosopher " << id << " finishes eating." << endl;
     }
 
     void think() {
-        isEating = false;
         cout << "Philosopher " << id << " is thinking." << endl;
+        sleep();
+    }
+
+    void sleep() {
+        // TODO: implement
     }
 };
 
@@ -25,11 +51,20 @@ int Philosopher::next_id = 0;
 int main() {
     cout << "Hello, World!" << endl;
     int numPhilosophers = 5;
+    
+    vector<Fork> forks(numPhilosophers);
 
     vector<Philosopher> philosophers;
     for (int i = 0; i < numPhilosophers; i++) {
-        philosophers.push_back(Philosopher());
+        int leftForkIndex = i;
+        int rightForkIndex = (i + 1) % numPhilosophers;
+        Philosopher philosopher = Philosopher(&forks[leftForkIndex], &forks[rightForkIndex]);
+
+        philosophers.push_back(philosopher);
     }
+
+    philosophers[0].eat();
+    philosophers[0].think();
 
     return 0;
 }
