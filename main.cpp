972f9839c67f914ee735cbd850216381836e479b
Jan Kazubski <jkazubski@icloud.com>
2025-03-22 15:36:00 +0100

    created Philosopher struct

--- main.cpp
+++ main.cpp
@@ -2,7 +2,34 @@
 
 using namespace std;
 
+struct Philosopher {
+    static int next_id;
+    int id;
+    bool isEating;
+    
+    Philosopher() : id(next_id++), isEating(false) { }
+
+    void eat() {
+        isEating = true;
+        cout << "Philosopher " << id << " is eating." << endl;
+    }
+
+    void think() {
+        isEating = false;
+        cout << "Philosopher " << id << " is thinking." << endl;
+    }
+};
+
+int Philosopher::next_id = 0;
+
 int main() {
     cout << "Hello, World!" << endl;
+    int numPhilosophers = 5;
+
+    vector<Philosopher> philosophers;
+    for (int i = 0; i < numPhilosophers; i++) {
+        philosophers.push_back(Philosopher());
+    }
+
     return 0;
 }
